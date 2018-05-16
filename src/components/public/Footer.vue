e4942d05017bb6b4780aadf245eb9148a94dc3c7
abundant <360930848@qq.com>
2018-05-16 17:51:36 +0800

    增加地区三级联动插件基于element-ui、收货人收货地址的添加功能完成

--- src/components/public/Footer.vue
+++ src/components/public/Footer.vue
@@ -75,260 +75,33 @@ export default {
     name: "footerView",
     data() {
         return {
-            brankImg: [
-                {
-                    name: "正品保证",
-                    explain: "国家认证 正规合法",
-                    img: require("../../assets/image/zheng1.png")
-                },
-                {
-                    name: "专业药师",
-                    explain: "用药全程指导 ",
-                    img: require("../../assets/image/zheng2.png")
-                },
-                {
-                    name: "厂家授权 ",
-                    explain: "厂家授权 正品渠道",
-                    img: require("../../assets/image/zheng6.png")
-                },
-                {
-                    name: "货到付款",
-                    explain: "货到付款 购药无忧",
-                    img: require("../../assets/image/zheng4.png")
-                },
-                {
-                    name: "隐私配送",
-                    explain: "安全放心 隐私配送",
-                    img: require("../../assets/image/zheng5.png")
-                },
-                {
-                    name: "满79元包邮",
-                    explain: "全场满79元包邮",
-                    img: require("../../assets/image/zheng3.png")
-                }
-            ],
-            wxImg: require("../../assets/image/wx1.png"),
-            //   说明
-            wxHlep: [
-                {
-                    title: "新手指南",
-                    text: [
-                        {
-                            name: "购物流程",
-                            url: "#"
-                        },
-                        {
-                            name: "积分说明",
-                            url: "#"
-                        },
-                        {
-                            name: "会员级别",
-                            url: "#"
-                        },
-                        {
-                            name: "优惠券",
-                            url: "#"
-                        },
-                        {
-                            name: "常见问题",
-                            url: "#"
-                        }
-                    ]
-                },
-                {
-                    title: "配送方式",
-                    text: [
-                        {
-                            name: "配送范围及运费",
-                            url: "#"
-                        },
-                        {
-                            name: "隐私配送",
-                            url: "#"
-                        },
-                        {
-                            name: "商品验收及签收",
-                            url: "#"
-                        }
-                    ]
-                },
-                {
-                    title: "支付方式",
-                    text: [
-                        {
-                            name: "货到付款",
-                            url: "#"
-                        },
-                        {
-                            name: "网上支付",
-                            url: "#"
-                        },
-                        {
-                            name: "银行转账",
-                            url: "#"
-                        }
-                    ]
-                },
-                {
-                    title: "售后服务",
-                    text: [
-                        {
-                            name: "退换货流程",
-                            url: "#"
-                        },
-                        {
-                            name: "退换货政策",
-                            url: "#"
-                        },
-                        {
-                            name: "退款流程",
-                            url: "#"
-                        }
-                    ]
-                },
-                {
-                    title: "特色服务",
-                    text: [
-                        {
-                            name: "会员及俱乐部",
-                            url: "#"
-                        },
-                        {
-                            name: "投诉建议",
-                            url: "#"
-                        },
-                        {
-                            name: "用药咨询",
-                            url: "#"
-                        },
-                        {
-                            name: "免费声明",
-                            url: "#"
-                        }
-                    ]
-                }
-            ],
-            //   联系电话
-            wxTel: [
-                { name: "药居士订购热线" },
-                { name: "400-8001-123" },
-                { name: "服务时间 08:30-23:00" },
-                { name: "药居士售后专线" },
-                { name: "020-89898981" },
-                { name: "服务时间 08:30-23:00" }
-            ],
-            //   政府链接
-            governmentLink: [
-                {
-                    name: "国家卫生和计划生育委员会",
-                    url: "http://www.nhfpc.gov.cn/"
-                },
-                {
-                    name: "国家食品药品监督管理总局",
-                    url: "http://www.sfda.gov.cn/WS01/CL0001/"
-                },
-                {
-                    name: "湖北省卫生和计划生育委员会",
-                    url: "http://www.hbwsjs.gov.cn/"
-                },
-                {
-                    name: "湖北省食品药品监督管理局",
-                    url: "http://www.hubfda.gov.cn/"
-                }
-            ],
-            //   友情链接
-            link: [
-                {
-                    name: "环球医药招商网",
-                    url: "http://www.qgyyzs.net"
-                },
-                {
-                    name: "皮肤病",
-                    url: "http://pfk.360kad.com/"
-                },
-                {
-                    name: "药品文章",
-                    url: "http://ypk.39.net"
-                },
-                {
-                    name: "摇篮育儿问答",
-                    url: "http://ask.yaolan.com/"
-                },
-                {
-                    name: "医药招商网",
-                    url: "http://www.yy960.com"
-                },
-                {
-                    name: "中华康网",
-                    url: "http://www.cnkang.com/"
-                },
-                {
-                    name: "69健康",
-                    url: "http://www.69jk.cn"
-                },
-                {
-                    name: "中药材",
-                    url: "http://www.zyctd.com/"
-                }
-            ],
-            // 关于我们
-            me: [
-                {
-                    name: "批发商洽",
-                    url: "#"
-                },
-                {
-                    name: "关于我们",
-                    url: "#"
-                },
-                {
-                    name: "掌声药店",
-                    url: "#"
-                },
-                {
-                    name: "实体药店",
-                    url: "#"
-                },
-                {
-                    name: "加入药居士",
-                    url: "#"
-                },
-                {
-                    name: "联系我们",
-                    url: "#"
-                },
-                {
-                    name: "经营认证",
-                    url: "#"
-                },
-                {
-                    name: "友情链接",
-                    url: "#"
-                },
-                {
-                    name: "TAG列表",
-                    url: "#"
-                },
-                {
-                    name: "网站地图",
-                    url: "#"
-                },
-                {
-                    name: "CPS联盟",
-                    url: "#"
-                }
-            ],
-            governmentData: [
-                {
-                    name: "国家食品药品监督管理总局-数据查询",
-                    url: "http://qy1.sfda.gov.cn/datasearch/face3/dir.html"
-                },
-                {
-                    name: "湖北省食品药品监督管理局-数据查询",
-                    url: "http://www.hubfda.gov.cn/info/iList.jsp?tm_id=254"
-                }
-            ]
+            brankImg: [],
+            wxImg: '',
+            wxHlep: [],
+            wxTel: [],
+            governmentLink: [],
+            link:[],
+            me:[],
+            governmentData:[]
         };
+    },
+    created(){
+        this.$ajax({
+            url: 'https://easy-mock.com/mock/5af8e2bb0d7ff97d1fdc9341/footer',
+            method: 'get'
+        }).then(response=>{
+            let data = response.data.data;
+            this.brankImg = data.brankImg;
+            this.wxImg = data.wxImg;
+            this.wxHlep = data.wxHlep;
+            this.wxTel = data.wxTel;
+            this.governmentLink = data.governmentLink;
+            this.link = data.link;
+            this.me = data.me;
+            this.governmentData = data.governmentData;
+        }).catch(error=>{
+            console.log(error)
+        })
     }
 };
 </script>
